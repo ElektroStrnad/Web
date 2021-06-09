cc233294c4accdbfccbd777625dcc8c5ffa6a1e5
ElektroStrnad <85556404+ElektroStrnad@users.noreply.github.com>
2021-06-09 08:12:54 +0200

    reload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 /*!
-* Start Bootstrap - Business Casual v7.0.1 (https://startbootstrap.com/theme/business-casual)
+* Start Bootstrap - Business Frontpage v5.0.2 (https://startbootstrap.com/template/business-frontpage)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
